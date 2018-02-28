22d474ed764ad081d074268fd11618af6240b54a
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-02-28 23:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var name = "Susan"
-var height = 40
-var message = `Susan is 74 inches tall` 
+var height = 39
+var message = 'Susan is 74 inches tall' 
 
 module.exports = {name, height, message}
 console.log(message)
